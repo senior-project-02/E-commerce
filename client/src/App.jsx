48e6a4e35729f45317afabb66aff67e3d26f368f
mootaznoomani <91527816+mootaznoomani@users.noreply.github.com>
2024-02-14 18:04:13 +0100

    change widthh

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
-import Productdetails from './componant mootaz/Productdetails.jsx'
+
 function App() {
 
   return (
     <>
-    <Productdetails/>
+
+     
+    
+
     </>
   )
 }
