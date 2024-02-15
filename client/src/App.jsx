e39a4ca0148e172bb33ab5570603dfba70d01475
amenibenabdallah <benabdallah2ameni@gmail.com>
2024-02-15 17:49:16 +0100

    some littels changes to avoid conflict

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,5 @@
 import React from 'react'
-// import SideBar from './Seller/SideBar'
-import ProductList from './Seller/ProductsList'
-import AddProduct from './Seller/AddProduct'
-import EditProduct from './Seller/EditProduct'
+
 
 
 
@@ -10,9 +7,7 @@ function App() {
 
   return (
     <>
-    <EditProduct/>
-      {/* <ProductList/> */}
-      {/* <AddProduct /> */}
+    
     </>
   )
 }
