66f376d447f15690bde26bed199ab53fb9d1261a
amenibenabdallah <benabdallah2ameni@gmail.com>
2024-02-15 09:14:11 +0100

    styling nav bar for the seller interface done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import SideBar from './Admin/SideBar'
+
+
 
 function App() {
 
   return (
     <>
-
-     
+   
+    <SideBar/>
     
 
     </>
--- server/database/indesx.js
+++ server/database/indesx.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2')
 
 
 
-const sequelize = new Sequelize('e-commerce', 'root', '22802888', {
+const sequelize = new Sequelize('e-commerce', 'root', 'asd12345hope@@', {
 
 
     host:'localhost',
