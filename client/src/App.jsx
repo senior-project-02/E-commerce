22404639dbc876668420f1f6cef0e6834278ac19
Ahmed-dh41 <dahechahmed10@gmail.com>
2024-02-15 14:50:40 +0100

    front sign up done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,13 @@
-import React from 'react'
-import './App.css'
+import React from 'react';
+import './App.css';
+import SignUp from './Ahmed/SignUp';
 
 function App() {
-  
-
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <SignUp />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -6,6 +6,8 @@ const crypto=require ('crypto');
 
 
 
+
+
 module.exports = {
 
   // sign in functions 
@@ -118,7 +120,25 @@ loginIn :function (req, res)  {
         }
     })
    
-}
+},
+// getUserData : (req, res) => {
+//   //verify the JWT token generated for the user
+//   jwt.verify(req.token, 'privatekey', (err, authorizedData) => {
+//     if(err){
+//       //If error send Forbidden (403)
+//       console.log('ERROR: Could not connect to the protected route');
+//       res.sendStatus(403);
+//     } else {
+//       //If token is successfully verified, we can send the autorized data 
+//       res.json({
+//         message: 'Successful log in',
+//         authorizedData
+//       });
+//       console.log('SUCCESS: Connected to protected route');
+//     }
+//   });
+// }
+  
 
 
 
