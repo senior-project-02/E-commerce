0a20512aadae26dc44bd15c7f1676ccbd1c6ddff
Ahmed-dh41 <dahechahmed10@gmail.com>
2024-02-15 18:26:21 +0100

    initialation router in the front end

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,27 @@
 
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './Ahmed/Login';
+import SignUp from './Ahmed/SignUp'
 
 
-
-
-function App() {
 
 
 function App() {
   return (
-    <>
+    <div>
 
-    </>
+<Router>
+      <Routes>
+
+      <Route path='/' element={<Login/>}/>
+      <Route path='/signup' element={<SignUp/>}/>
+
+      </Routes>
+      </Router>
+      
+    </div>
   );
 }
 
-export default App;
+export default App
