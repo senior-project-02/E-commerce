8b87000935582ca149232cc52555972513beba57
amenibenabdallah <benabdallah2ameni@gmail.com>
2024-02-15 20:17:04 +0100

    intiliazing router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,34 @@
-
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './Ahmed/Login';
+import SignUp from './Ahmed/SignUp'
+import Home from './Aymen/Home'
+import Detailsproduct from './componant mootaz/Detailsproduct';
+import ProductsList from './Seller/ProductsList'
 
 
-
-
-function App() {
 
 
 function App() {
   return (
-    <>
+    <div>
 
-    </>
+<Router>
+      <Routes>
+
+      <Route path='/' element={<Home/>}/>
+      <Route path='/productDetails' element={<Detailsproduct/>}/>
+      
+      <Route path='/Login' element={<Login/>}/>
+      <Route path='/signup' element={<SignUp/>}/>
+      <Route path='/Seller' element={<ProductsList/>}/>
+      
+      </Routes>
+      </Router>
+
+    </div>
+
   );
 }
 
-export default App;
+export default App
--- server/routes/cart.js
+++ server/routes/cart.js
@@ -6,7 +6,7 @@ router.get('/someproduct/:id',controller.Somecategories)
 router.get('/product',controller.Allproduct)
 router.get('/cart/:id',controller.Cart)
 router.post('/createcart',controller.Createcart)
-router.put('/updatecart/:id',controller.UpdateCart)
+router.put('/updatecart/:id',controller.Updatecart)
 router.post('/carthasp',controller.Insertcarthasproduct)
 router.get('/carthasp/:id',controller.Productfromcard)
 router.delete("/removeproduct/:cartid/:productid",controller.Removeproductfromcart);
