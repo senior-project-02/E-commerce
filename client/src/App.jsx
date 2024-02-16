f473d702cf48ab1bab30e3f6a56e090b715bdb0a
jabril205 <jabrilslim@gmail.com>
2024-02-16 18:15:57 +0100

    Home page done part 1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,32 +12,8 @@ import AddProduct from './Seller/AddProduct'
 
 
 
-import Productcart from "./componant mootaz/Productcart"
-import Productdetails from './componant mootaz/Productdetails'
-
-
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './Ahmed/Login';
-import SignUp from './Ahmed/SignUp'
-import Home from './Aymen/Home'
-import Detailsproduct from './componant mootaz/Detailsproduct';
-import ProductsList from './Seller/ProductsList'
-import Dashboard from './Seller/SellerInterface';
-import AddProduct from './Seller/AddProduct'
-
-
-
-
-
-
 function App() {
   return (
-
-    <>
-    <Productcart/>
-     
-    
-
     <div>
 
 <Router>
@@ -45,12 +21,12 @@ function App() {
 
       <Route path='/' element={<Home/>}/>
       <Route path='/productDetails' element={<Detailsproduct/>}/>
-      
+
       <Route path='/Login' element={<Login/>}/>
       <Route path='/signup' element={<SignUp/>}/>
       <Route path='/Seller' element={<Dashboard/>}/>
       <Route path='/Seller/AddProduct' element={<AddProduct/>}/>
-      
+
       </Routes>
       </Router>
 
