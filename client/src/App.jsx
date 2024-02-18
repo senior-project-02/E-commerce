7c8a93d53b15a67bbbae70739d57ac1f2e65a218
jabril205 <jabrilslim@gmail.com>
2024-02-18 19:58:18 +0100

    done 1 and  more is coming

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,3 @@
-
-
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './Ahmed/Login';
@@ -7,10 +5,14 @@ import SignUp from './Ahmed/SignUp'
 import Home from './Aymen/Home'
 import Detailsproduct from './componant mootaz/Detailsproduct';
 import ProductsList from './Seller/ProductsList'
-import Dashboard from './Seller/SellerInterface';
+import Seller from './Seller/SellerInterface';
 import AddProduct from './Seller/AddProduct'
 import EditProduct from './Seller/EditProduct'
-
+import SideBar from './Admin/SideBar';
+import AllTable from './Admin/AllTable';
+import EdditRole from './Admin/EditRole';
+import CategoryList from './Admin/Category';
+import Wishlist from "./Aymen/wishList/WishList"
 
 
 
@@ -20,26 +22,37 @@ function App() {
   return (
     <div>
 
-      
-      
+
+
 <Router>
       <Routes>
 
       <Route path='/' element={<Home/>}/>
+      
+      <Route path="/wishList" element={<Wishlist/>}/>
       <Route path='/productDetails' element={<Detailsproduct/>}/>
 
       <Route path='/Login' element={<Login/>}/>
       <Route path='/signup' element={<SignUp/>}/>
-      <Route path='/Seller' element={<Dashboard/>}/>
+      <Route path='/Seller' element={<Seller/>}/>
       <Route path='/Seller/AddProduct' element={<AddProduct/>}/>
 
       <Route path='/Seller/EditProduct' element={<EditProduct/>}/>
-      <Route path='/Seller/ProductList' element={<Dashboard/>}/>
-      
-      
+      <Route path='/Seller/ProductList' element={<Seller/>}/>
+
+      <Route path='/Admin/All' element={<AllTable/>}/>
+      <Route path='/Admin/EditRole' element={<EdditRole/>}/>
+      <Route path='/Admin/CategoryList' element={<CategoryList/>}/>
+
+
+
+
+
+
+
 
       </Routes>
-      </Router> 
+      </Router>
 
     </div>
 
@@ -47,5 +60,3 @@ function App() {
 }
 
 export default App
-
-
